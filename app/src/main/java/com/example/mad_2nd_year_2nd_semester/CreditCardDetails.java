4b6b45766bc7132cc525ef3d54cef8cd91baa441
SachiniRasanga <sachisach983@gmail.com>
2021-08-14 13:30:07 +0530

    third Commit

---
+++ app/src/main/java/com/example/mad_2nd_year_2nd_semester/CreditCardDetails.java
@@ -0,0 +1,14 @@
+package com.example.mad_2nd_year_2nd_semester;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreditCardDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_card_details);
+    }
+}
--- app/src/main/java/com/example/mad_2nd_year_2nd_semester/MainActivity.java
+++ app/src/main/java/com/example/mad_2nd_year_2nd_semester/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
              @Override
              public void onClick(View v){
 
-                Intent i=new Intent(MainActivity.this,MasterCardpage.class);
+                Intent i=new Intent(MainActivity.this,CreditCardDetails.class);
                 startActivity(i);
 
              }
